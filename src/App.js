1031f7e0d8c9ed116a51f3adfa3f13c9dfe112a8
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-01-06 12:00:25 -0500

    grab geo data from window and linkd in profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  // let lastRendered = window.document.lastModified;
+
+  // 3 params: success cb, error cb, postionOptions Object
+  const getLocationData = () => {
+    return window.navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      let accuracy = position.coords.accuracy;
+      console.log('lat: ', lat, 'lon: ', lon, 'accuracy: ', accuracy)
+    }, (error) => {
+      console.log(error)
+    },
+      { enableHighAccuracy: true, timeout: 3000 }
+    )
+  };
+  const link = () => {
+    fetch('https://api.linkedin.com/v1/people-search?first-name=Tom')
+    .then(results => {
+      console.log(results.json())
+    })
+  }
+  link()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Nav</h1>
+        <h1>Geolocate</h1>
+      <h1>Footer</h1>
+      <button onClick={getLocationData}>click me</button>
+    </section>
   );
 }
 
