af1b57e8256aeec13495b7293b3a327e4211179e
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-01-17 15:02:25 -0500

    added footer and navbar components to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import routes from './routes';
 
+// components
+import Navbar from './features/navbar';
+import Footer from './features/footer';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <section id="app">
         {routes}
       </section>
+      <Footer />
     </Router>
   );
 }
